2f3c0a7bc7f7bd7f5e99c3be56f1fdcadac8babc
MEziliano <murilosez06@gmail.com>
2022-08-23 12:00:28 -0300

    Add Variables

---
+++ Variables.cpp
@@ -0,0 +1,17 @@
+/*
+This concept is the same as any other program leanguage 
+Let's chech, what we can run with C++*/
+#include <iostream>
+#include <tuple>
+using namespace std;
+
+int main() {
+  // the syntax is: type variableName = value;
+  int    MyNum = 26;             // Integer (whole number without decimals)
+  double MyFloatNum = 1.93;      // Floating point number (with decimals)
+  char   MyLetter = 'M';         // Character
+  string MyText = "Hello World"; // String (text)
+  bool   MyBoolean = true;       // Boolean (true or false)
+  cout << MyText; // but the cout only return on 
+  return 0;
+}
