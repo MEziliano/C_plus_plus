73648d06d0f6db0f4e9b7eaa16112c74372d13d2
Murilo Eziliano <77080184+MEziliano@users.noreply.github.com>
2024-06-07 16:57:06 -0300

    Update 01_02.cpp

--- course/01_02.cpp
+++ course/01_02.cpp
@@ -4,5 +4,12 @@
 #include <iostream>
 #include <string>
 
-inht main(){
+int main(){
   std:: string str;
+  std:: cout << "Enter your name:" <<std::flush;
+  std:: cin >> str;
+  std:: cout<< "Nice to meet you, "<< str << "!" << std::endl;
+  
+  std:: cout << std::endl <<std:: endl;
+  return (0);
+}
